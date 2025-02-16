a42d776a604ab982667922e033c77b16c5d388a2
JaneConan <JaneConan@live.com>
2025-02-16 21:04:04 +0800

    优化相关接口

--- src/AntSK.AppHost/Program.cs
+++ src/AntSK.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.AntSK>("antsk");
 
+builder.AddProject<Projects.AnyShareHandling>("anysharehandling");
+
 builder.Build().Run();
---
+++ src/TestConsoleApp/JwtGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JWT;
+using JWT.Algorithms;
+using JWT.Builder;
+
+public class JwtGenerator
+{
+    public static string GetToken(string username, string secret)
+    {
+        var expiration = DateTimeOffset.UtcNow.ToUnixTimeSeconds() + 60;
+
+        var token = JwtBuilder.Create()
+            .WithAlgorithm(new HMACSHA256Algorithm())
+            .WithSecret(secret)
+            .AddClaim("username", username)
+            .AddClaim("exp", expiration)
+            .Encode();
+
+        return token;
+    }
+}
